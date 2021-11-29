305f9bc8145e200bc504d86526ec9b053650e6fd
Mridul Kumar Shrivastava <smridul@oath.com>
2021-11-29 13:58:41 -0800

    backtraking

--- MySolutions/myanswers/CombinationSumII.java
+++ MySolutions/myanswers/CombinationSumII.java
@@ -10,12 +10,21 @@ import java.util.Map;
 public class CombinationSumII {
     public List<List<Integer>> combinationSum(int[] candidates, int target) {
 
+
+        Map<Integer, Integer> countMap = new HashMap<>();
+        Map<Integer, Integer> countMapIndex = new HashMap<>();
+
+        for (int i = 0; i < candidates.length; i++) {
+            countMap.put(candidates[i], countMap.getOrDefault(candidates[i], 0) + 1);
+            countMapIndex.put(i, countMap.get(candidates[i]));
+        }
+
+
         List<List<Integer>> result = new ArrayList<>();
-        combinationSumRecursionHelper(candidates, target, 0, result, new ArrayList<>(), new HashMap<>());
+        combinationSumRecursionHelper2(candidates, target, 0, result, new ArrayList<>(), new HashMap<>(), countMapIndex);
         int a = 0;
         return result;
     }
-
 
 
     // each number can be used multiple times
@@ -34,28 +43,69 @@ public class CombinationSumII {
         //case 1 selected nums[i]
 
         boolean added = false;
-        if(!map.containsKey(candidates[index]) || map.get(candidates[index]) == index) {
+        if (!map.containsKey(candidates[index]) || map.get(candidates[index]) == index) {
             current.add(candidates[index]);
-             added = true;
-             map.put(candidates[index], index);
+            added = true;
+            map.put(candidates[index], index);
             combinationSumRecursionHelper(candidates, target - candidates[index], index, result, current, map);
         }
 
         //case 2 selected nums[i] not selected
-        if(added == true) {
+        if (added == true) {
             current.remove(current.size() - 1);
         }
-        combinationSumRecursionHelper(candidates, target, index+1, result, current, map);
+        combinationSumRecursionHelper(candidates, target, index + 1, result, current, map);
+    }
+
+
+    // each number can be used 1 times
+    public void combinationSumRecursionHelper2(int[] candidates, int target, int index,
+                                               List<List<Integer>> result, List<Integer> current, Map<Integer, Integer> currentCountMap, Map<Integer, Integer> countMapIndex) {
+        if (target < 0) {
+            return;
+        }
+        if (target == 0) {
+            result.add(new ArrayList<>(current));
+            return;
+        }
+        if (index == candidates.length) {
+            return;
+        }
+        boolean added = false;
+
+        if (countMapIndex.get(index) == currentCountMap.getOrDefault(candidates[index], 0) +1) {
+            current.add(candidates[index]);
+            currentCountMap.put(candidates[index], currentCountMap.getOrDefault(candidates[index], 0)+1);
+            added = true;
+            combinationSumRecursionHelper2(candidates, target - candidates[index], index + 1, result, current, currentCountMap, countMapIndex);
+        }
+
+        //case 2 selected nums[i] not selected
+        if (added == true) {
+            current.remove(current.size() - 1);
+            currentCountMap.put(candidates[index], currentCountMap.getOrDefault(candidates[index], 1)-1);
+        }
+        combinationSumRecursionHelper2(candidates, target, index + 1, result, current, currentCountMap, countMapIndex);
     }
 
     @Test
     public void test() {
 
-        int[] nums = new int[]{10,1,2,7,6,1,5};
+        int[] nums = new int[]{10, 1, 2, 7, 6, 1, 5};
         List<List<Integer>> res = combinationSum(nums, 8);
 
         for (List l : res) {
             System.out.println(l);
         }
+    }
+}
+
+class Pair1 {
+    int firstOccurence;
+    int index;
+
+    public Pair1(int a, int b) {
+        firstOccurence = a;
+        index = b;
     }
 }
